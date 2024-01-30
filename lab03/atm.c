54840de495687b1692a322a9735240f5a45eadb8
Joey Huang <joeyh6547@gmail.com>
2024-01-30 18:41:11 -0500

    .

---
+++ lab03/atm.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+    puts("Welcome to \"Temple ATM\"");
+
+    puts("Thanks for using \"Temple ATM\"");
+    exit(0);
+}
