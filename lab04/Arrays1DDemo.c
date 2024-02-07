4d40428b2c3289e169d7b38aa0adb1f02d85030b
Joey Huang <joeyh6547@gmail.com>
2024-02-06 21:37:53 -0500

    .

---
+++ lab04/Arrays1DDemo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* Joey Huang
+ * 2/06/2024
+ * CIS 2107
+ * Lab 04 1D Arrays 
+ * 
+ * Write a C Program for an ATM machine by implementing functions
+ */
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
