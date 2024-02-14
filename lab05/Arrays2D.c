b5c74c09a90cbce8c733149c4b1e8d52944b68c8
Joey Huang <joeyh6547@gmail.com>
2024-02-14 11:05:00 -0500

    lab5

---
+++ lab05/Arrays2D.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int max() 
+int rowSum()
+int columnSum ()
+int isSquare ()
+int displayOutputs()
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
+
+int max() 
+int rowSum()
+int columnSum ()
+int isSquare ()
+int displayOutputs()
