6953e5b93a0fd66916fdd8ee9141fbc00b5c6d03
Joey Huang <joeyh6547@gmail.com>
2024-02-20 12:04:56 -0500

    lab06

---
+++ lab06/ArraysofPointers.c
@@ -0,0 +1,36 @@
+/* 
+Name: Joey Huang 
+Date: 02/20/2024
+Course: CSI2107 
+HW#: Lab 05: ““Arrays of Pointers to Functions” 
+
+Objective: To design and implement array of function pointer. 
+
+The purpose of this program is to modify existing code related to examslecting 
+grades for individual students in a 2D matrix and determining the minimum, maximum, and each average. 
+
+// Each of the functions has also been modified to allow for a
+// menu-driven interface. This interface is an array of pointers to the functions.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+void printArray(int students, int exams, int grades[students][exams]);
+void minimum(int students, int exams, int grades[students][exams]);
+void maximum(int students, int exams, int grades[students][exams]);
+void average(int students, int exams, int grades[students][exams]);
+
+int main()
+{
+    
+}
+
+void printArray(int students, int exams, int grades[students][exams]){
+}
+void minimum(int students, int exams, int grades[students][exams]){
+}
+void maximum(int students, int exams, int grades[students][exams]){
+}
+void average(int students, int exams, int grades[students][exams]){
+}
